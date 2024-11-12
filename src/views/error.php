6089dc0c78619f81f4c7ad1018222980cd71473f
Daniel Kipp <daniel.kipp@gmail.com>
2024-11-12 15:16:24 +0100

    implemented error handling

---
+++ src/views/error.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Views\error;
+
+?>
+
+<div class="alert alert-danger m-4" role="alert">
+    <h4 class="alert-heading">Upps...!</h4>
+    <p>Leider ist etwas schief gelaufen. <br>
+    Bitte versuch es erneut oder wende dich an einen Administrator.</p>
+</div>
